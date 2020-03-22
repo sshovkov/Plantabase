036dcb62d78f0ffa8157a33aa0151d7164d3b4eb
Sophia Shovkovy <sshovkov@asu.edu>
2020-03-21 18:12:17 -0700

    Basic features implemented

---
+++ Plantabase/CreateProfileViewController.swift
@@ -0,0 +1,30 @@
+//
+//  CreateProfileViewController.swift
+//  Plantabase
+//
+//  Created by Shovkovy on 3/21/20.
+//  Copyright © 2020 Shovkovy. All rights reserved.
+//
+
+import UIKit
+
+class CreateProfileViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
